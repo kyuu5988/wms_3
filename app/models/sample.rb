48484a3815224604552ccad32edd6a0639f1d59b
kyuu5988 <kyuu5988@yahoo.co.jp>
2020-09-05 22:22:46 +0900

    新規作成の正規表現完成

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -12,12 +12,26 @@ class Sample < ApplicationRecord
 
   with_options length:{ minimum: 10, message:'10桁ありません' }, format: { with: /\A\d{10}/,message: '半角数字のみ登録可' } do
     validates :基本コード
+    #validates :カラム名
+    #複数のカラムが同じ条件ならこの方法で
   end
 
   validates :サブコード, length:{ minimum: 3, message:'3桁ありません' }, format: { with: /\A\d{3}/, message: '半角数字のみ登録可' }
+  #複数のカラムが別々の条件なら1つずつ設定
+
+  validates :個別番号, length:{ minimum: 1, message:'入力必須' }, format: { with: /\A\d{1}/, message: '半角数字のみ登録可' }
+
+  validates :申込番号, length:{ minimum: 6, message:'6桁ありません' }, format: { with: /\A\d{6}/, message: '半角数字のみ登録可' }
+
+  validates :販売商品名, presence: true
+
+  validates :カテゴリー, length:{ minimum: 2, message:'2桁ありません' }, format: { with: /\A\d{2}/, message: '半角数字のみ登録可' }
+
+  validates :ロケーション, format: { with: /\A\w+\z/, message: '半角英数字のみ登録可' }
 
 
-  validates :個別番号, :申込番号, :販売商品名, :カテゴリー, :ロケーション, presence: true
+
+  #validates :カテゴリー, :ロケーション, presence: true
 
 
 
