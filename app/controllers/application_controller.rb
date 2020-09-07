b1c695f9a43e00a49e90cf6dd89869f866ceb481
kyuu5988 <kyuu5988@yahoo.co.jp>
2020-09-07 22:45:14 +0900

    user新規登録へのリンクをオリジナル化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
-
+  # ↓ログインしないとAppを利用できないようにする
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-
+  # userテーブルにカラムを追加したらここでデータ保存の許可をする↓
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:group])
   end
 
 end
