2fc99b27998ae09f34dd28f3aa13be993c364a77
kyuu5988 <kyuu5988@yahoo.co.jp>
2020-09-08 10:18:28 +0900

    パスワード手動リセット仮設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  #パスワードリセット関連設定 テスト記述
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #↑を設定したらサーバー再起動
+
 end
