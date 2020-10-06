e7d6d1002ebbf5e35027e81a0d19ad34c919af04
kyuu5988 <kyuu5988@yahoo.co.jp>
2020-10-07 00:36:56 +0900

    合計air_time表示

--- app/controllers/air_times_controller.rb
+++ app/controllers/air_times_controller.rb
@@ -3,9 +3,6 @@ class AirTimesController < ApplicationController
   def create
     air_time = AirTime.create(air_time_params)
     #onair_t計算実験
-    # air_time = AirTime.find()
-    # total = air_time.end_t - air_time.start_t
-    # air_time.update(onair_t: total)
 
     flash[:onair] = "OA予定を追加しました。"
     redirect_to "/samples/#{params[:sample_id]}"
@@ -14,9 +11,22 @@ class AirTimesController < ApplicationController
   private
   def air_time_params
     #test
-    # total = params[:air_time][:end_t]
+    et4 = params[:air_time]["end_t(4i)"].to_i
+    et5 = params[:air_time]["end_t(5i)"].to_i
+    et6 = params[:air_time]["end_t(6i)"].to_i
 
-    params.require(:air_time).permit(:date, :start_t, :end_t, :onair_t).merge(sample_id: params[:sample_id])
+    st4 = params[:air_time]["start_t(4i)"].to_i
+    st5 = params[:air_time]["start_t(5i)"].to_i
+    st6 = params[:air_time]["start_t(6i)"].to_i
+
+    tt4 = et4 - st4
+    tt5 = et5 - st5
+    tt6 = et6 - st6
+
+    total = "#{tt4}:#{tt5}:#{tt6}"
+    
+
+    params.require(:air_time).permit(:date, :start_t, :end_t).merge(sample_id: params[:sample_id], onair_t: total)
     
   end
 
