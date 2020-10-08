97e9695664f0e74f507fdf7b8d1333b6dc70a6bf
kyuu5988 <kyuu5988@yahoo.co.jp>
2020-10-08 21:52:28 +0900

    passリセットメール仮設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,5 +63,17 @@ Rails.application.configure do
   #パスワードリセット関連設定 テスト記述
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   #↑を設定したらサーバー再起動
+  #以下、テスト中
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => "kyuu5988@gmail.com",
+    :password => "xmgwuoxdvsliufua",
+    :authentication => 'login'
+}
 
 end
