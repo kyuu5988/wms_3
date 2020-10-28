e0759ea9bfe6b4d5642986cc29605125e9c68e6b
kyuu5988 <kyuu5988@yahoo.co.jp>
2020-10-28 17:24:16 +0900

    Revert "アカウント新規登録 test code 完成"
    This reverts commit a3da53e6b61bc07c0b932f02de2268d57bffbf92.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,5 @@
 FactoryBot.define do
   factory :user do
-    name              {Faker::Name.initials(number: 4)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 8)}
-    password_confirmation {password}
+    
   end
 end
