e91c607980816b7474991febfccc75f33a3db1e0
kyuu5988 <kyuu5988@yahoo.co.jp>
2020-10-28 09:23:30 +0900

    pass制限変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
    
-   
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: '英字と数字を含めて入力して下さい'
+       
+
+
   validates :name, presence: true
 
   has_many :resumes  # resumesテーブルとのアソシエーション
