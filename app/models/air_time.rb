4814042fd028a48e6011d59e1c32a2ca5320ac0d
kyuu5988 <kyuu5988@yahoo.co.jp>
2020-10-28 17:22:21 +0900

    Revert "sample登録 test code 完成"
    This reverts commit d659d1c161bc606375d86ab1c3b5e6eb399ae5ce.

--- app/models/air_time.rb
+++ app/models/air_time.rb
@@ -1,2 +1,5 @@
 class AirTime < ApplicationRecord
+  belongs_to :sample #テスト中
+  #バリデーション検証
+  
 end
--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,2 +1,8 @@
 class Resume < ApplicationRecord
+  #現状、正確にはアソシエーションでは無いので、設定なし
+  #設定すると保存出来ない（解決方法不明）
+  # belongs_to :sample  # samplesテーブルとのアソシエーション
+  # belongs_to :user  # usersテーブルとのアソシエーション
+
+  #バリデーションの設定は不要。必要な値を取得して必要なカラムに保存する設定（コントローラーで）  
 end
