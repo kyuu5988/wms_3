0fb7b0fdb2a55a012d5f1c2957c5abfd5727ece0
kyuu5988 <kyuu5988@yahoo.co.jp>
2020-11-06 16:21:25 +0900

    user情報

--- app/javascript/hides/hide_menu_select.js
+++ app/javascript/hides/hide_menu_select.js
@@ -6,6 +6,7 @@ function hide_menu_select(){
   const expMenu3 = document.getElementById("exp-menu3")
   const expMenu4 = document.getElementById("exp-menu4")
   const expMenu5 = document.getElementById("exp-menu5")
+  const expMenu6 = document.getElementById("exp-menu6")
 
   const insideMenu1 = document.getElementById("inside-menu1")
   const insideMenu2 = document.getElementById("inside-menu2")
@@ -20,6 +21,11 @@ function hide_menu_select(){
       insideMenu1.setAttribute("style", "display:block;");
       insideMenu2.removeAttribute("style", "display:block;");
       insideMenu3.removeAttribute("style", "display:block;");
+      insideMenu4.removeAttribute("style", "display:block;");
+      insideMenu5.removeAttribute("style", "display:block;");
+      insideMenu6.removeAttribute("style", "display:block;");
+      insideMenu7.removeAttribute("style", "display:block;");
+
     });
 
     expMenu2.addEventListener('click',function(){
@@ -27,7 +33,10 @@ function hide_menu_select(){
       insideMenu2.setAttribute("style", "display:block;");
       insideMenu1.removeAttribute("style", "display:block;");
       insideMenu3.removeAttribute("style", "display:block;");
-
+      insideMenu4.removeAttribute("style", "display:block;");
+      insideMenu5.removeAttribute("style", "display:block;");
+      insideMenu6.removeAttribute("style", "display:block;");
+      insideMenu7.removeAttribute("style", "display:block;");
     });  
 
     expMenu3.addEventListener('click',function(){
@@ -35,8 +44,22 @@ function hide_menu_select(){
       insideMenu3.setAttribute("style", "display:block;");
       insideMenu1.removeAttribute("style", "display:block;");
       insideMenu2.removeAttribute("style", "display:block;");
+      insideMenu4.removeAttribute("style", "display:block;");
+      insideMenu5.removeAttribute("style", "display:block;");
+      insideMenu6.removeAttribute("style", "display:block;");
+      insideMenu7.removeAttribute("style", "display:block;");
     });
 
+    expMenu4.addEventListener('click',function(){
+      console.log('Menu3 open');
+      insideMenu4.setAttribute("style", "display:block;");
+      insideMenu1.removeAttribute("style", "display:block;");
+      insideMenu2.removeAttribute("style", "display:block;");
+      insideMenu3.removeAttribute("style", "display:block;");
+      insideMenu5.removeAttribute("style", "display:block;");
+      insideMenu6.removeAttribute("style", "display:block;");
+      insideMenu7.removeAttribute("style", "display:block;");
+    });
 
 }
 
