6ada7030ac30f1e5f5a35469be28e322abd4b59d
kyuu5988 <kyuu5988@yahoo.co.jp>
2020-11-06 19:16:37 +0900

    js menu表示・隠し全部

--- app/javascript/hides/hide_menu_select.js
+++ app/javascript/hides/hide_menu_select.js
@@ -11,6 +11,11 @@ function hide_menu_select(){
   const expMenu8 = document.getElementById("exp-menu8")
   const expMenu9 = document.getElementById("exp-menu9")
   const expMenu10 = document.getElementById("exp-menu10")
+  const expMenu11 = document.getElementById("exp-menu11")
+  const expMenu12 = document.getElementById("exp-menu12")
+  const expMenu13 = document.getElementById("exp-menu13")
+  const expMenu14 = document.getElementById("exp-menu14")
+  const expMenu15 = document.getElementById("exp-menu15")
 
   const insideMenu1 = document.getElementById("inside-menu1")
   const insideMenu2 = document.getElementById("inside-menu2")
@@ -22,7 +27,11 @@ function hide_menu_select(){
   const insideMenu8 = document.getElementById("inside-menu8")
   const insideMenu9 = document.getElementById("inside-menu9")
   const insideMenu10 = document.getElementById("inside-menu10")
-
+  const insideMenu11 = document.getElementById("inside-menu11")
+  const insideMenu12 = document.getElementById("inside-menu12")
+  const insideMenu13 = document.getElementById("inside-menu13")
+  const insideMenu14 = document.getElementById("inside-menu14")
+  const insideMenu15 = document.getElementById("inside-menu15")
 
     expMenu1.addEventListener('click',function(){
       console.log('Menu1 open');
@@ -36,7 +45,11 @@ function hide_menu_select(){
       insideMenu8.removeAttribute("style", "display:block;");
       insideMenu9.removeAttribute("style", "display:block;");
       insideMenu10.removeAttribute("style", "display:block;");
-
+      insideMenu11.removeAttribute("style", "display:block;");
+      insideMenu12.removeAttribute("style", "display:block;");
+      insideMenu13.removeAttribute("style", "display:block;");
+      insideMenu14.removeAttribute("style", "display:block;");
+      insideMenu15.removeAttribute("style", "display:block;");
     });
 
     expMenu2.addEventListener('click',function(){
@@ -51,6 +64,11 @@ function hide_menu_select(){
       insideMenu8.removeAttribute("style", "display:block;");
       insideMenu9.removeAttribute("style", "display:block;");
       insideMenu10.removeAttribute("style", "display:block;");
+      insideMenu11.removeAttribute("style", "display:block;");
+      insideMenu12.removeAttribute("style", "display:block;");
+      insideMenu13.removeAttribute("style", "display:block;");
+      insideMenu14.removeAttribute("style", "display:block;");
+      insideMenu15.removeAttribute("style", "display:block;");
     });  
 
     expMenu3.addEventListener('click',function(){
@@ -65,6 +83,11 @@ function hide_menu_select(){
       insideMenu8.removeAttribute("style", "display:block;");
       insideMenu9.removeAttribute("style", "display:block;");
       insideMenu10.removeAttribute("style", "display:block;");
+      insideMenu11.removeAttribute("style", "display:block;");
+      insideMenu12.removeAttribute("style", "display:block;");
+      insideMenu13.removeAttribute("style", "display:block;");
+      insideMenu14.removeAttribute("style", "display:block;");
+      insideMenu15.removeAttribute("style", "display:block;");
     });
 
     expMenu4.addEventListener('click',function(){
@@ -79,6 +102,11 @@ function hide_menu_select(){
       insideMenu8.removeAttribute("style", "display:block;");
       insideMenu9.removeAttribute("style", "display:block;");
       insideMenu10.removeAttribute("style", "display:block;");
+      insideMenu11.removeAttribute("style", "display:block;");
+      insideMenu12.removeAttribute("style", "display:block;");
+      insideMenu13.removeAttribute("style", "display:block;");
+      insideMenu14.removeAttribute("style", "display:block;");
+      insideMenu15.removeAttribute("style", "display:block;");
     });
 
     expMenu5.addEventListener('click',function(){
@@ -93,6 +121,11 @@ function hide_menu_select(){
       insideMenu8.removeAttribute("style", "display:block;");
       insideMenu9.removeAttribute("style", "display:block;");
       insideMenu10.removeAttribute("style", "display:block;");
+      insideMenu11.removeAttribute("style", "display:block;");
+      insideMenu12.removeAttribute("style", "display:block;");
+      insideMenu13.removeAttribute("style", "display:block;");
+      insideMenu14.removeAttribute("style", "display:block;");
+      insideMenu15.removeAttribute("style", "display:block;");
     });
 
     expMenu6.addEventListener('click',function(){
@@ -107,6 +140,11 @@ function hide_menu_select(){
       insideMenu8.removeAttribute("style", "display:block;");
       insideMenu9.removeAttribute("style", "display:block;");
       insideMenu10.removeAttribute("style", "display:block;");
+      insideMenu11.removeAttribute("style", "display:block;");
+      insideMenu12.removeAttribute("style", "display:block;");
+      insideMenu13.removeAttribute("style", "display:block;");
+      insideMenu14.removeAttribute("style", "display:block;");
+      insideMenu15.removeAttribute("style", "display:block;");
     });
 
     expMenu7.addEventListener('click',function(){
@@ -121,10 +159,15 @@ function hide_menu_select(){
       insideMenu8.removeAttribute("style", "display:block;");
       insideMenu9.removeAttribute("style", "display:block;");
       insideMenu10.removeAttribute("style", "display:block;");
+      insideMenu11.removeAttribute("style", "display:block;");
+      insideMenu12.removeAttribute("style", "display:block;");
+      insideMenu13.removeAttribute("style", "display:block;");
+      insideMenu14.removeAttribute("style", "display:block;");
+      insideMenu15.removeAttribute("style", "display:block;");
     });
 
     expMenu8.addEventListener('click',function(){
-      console.log('Menu7 open');
+      console.log('Menu8 open');
       insideMenu8.setAttribute("style", "display:block;");
       insideMenu1.removeAttribute("style", "display:block;");
       insideMenu2.removeAttribute("style", "display:block;");
@@ -135,9 +178,146 @@ function hide_menu_select(){
       insideMenu7.removeAttribute("style", "display:block;");
       insideMenu9.removeAttribute("style", "display:block;");
       insideMenu10.removeAttribute("style", "display:block;");
+      insideMenu11.removeAttribute("style", "display:block;");
+      insideMenu12.removeAttribute("style", "display:block;");
+      insideMenu13.removeAttribute("style", "display:block;");
+      insideMenu14.removeAttribute("style", "display:block;");
+      insideMenu15.removeAttribute("style", "display:block;");
     });
 
+    expMenu9.addEventListener('click',function(){
+      console.log('Menu9 open');
+      insideMenu9.setAttribute("style", "display:block;");
+      insideMenu1.removeAttribute("style", "display:block;");
+      insideMenu2.removeAttribute("style", "display:block;");
+      insideMenu3.removeAttribute("style", "display:block;");
+      insideMenu4.removeAttribute("style", "display:block;");
+      insideMenu5.removeAttribute("style", "display:block;");
+      insideMenu6.removeAttribute("style", "display:block;");
+      insideMenu7.removeAttribute("style", "display:block;");
+      insideMenu8.removeAttribute("style", "display:block;");
+      insideMenu10.removeAttribute("style", "display:block;");
+      insideMenu11.removeAttribute("style", "display:block;");
+      insideMenu12.removeAttribute("style", "display:block;");
+      insideMenu13.removeAttribute("style", "display:block;");
+      insideMenu14.removeAttribute("style", "display:block;");
+      insideMenu15.removeAttribute("style", "display:block;");
+    });
 
-}
+    expMenu10.addEventListener('click',function(){
+      console.log('Menu10 open');
+      insideMenu10.setAttribute("style", "display:block;");
+      insideMenu1.removeAttribute("style", "display:block;");
+      insideMenu2.removeAttribute("style", "display:block;");
+      insideMenu3.removeAttribute("style", "display:block;");
+      insideMenu4.removeAttribute("style", "display:block;");
+      insideMenu5.removeAttribute("style", "display:block;");
+      insideMenu6.removeAttribute("style", "display:block;");
+      insideMenu7.removeAttribute("style", "display:block;");
+      insideMenu8.removeAttribute("style", "display:block;");
+      insideMenu9.removeAttribute("style", "display:block;");
+      insideMenu11.removeAttribute("style", "display:block;");
+      insideMenu12.removeAttribute("style", "display:block;");
+      insideMenu13.removeAttribute("style", "display:block;");
+      insideMenu14.removeAttribute("style", "display:block;");
+      insideMenu15.removeAttribute("style", "display:block;");
+    });
+
+    expMenu11.addEventListener('click',function(){
+      console.log('Menu11 open');
+      insideMenu11.setAttribute("style", "display:block;");
+      insideMenu1.removeAttribute("style", "display:block;");
+      insideMenu2.removeAttribute("style", "display:block;");
+      insideMenu3.removeAttribute("style", "display:block;");
+      insideMenu4.removeAttribute("style", "display:block;");
+      insideMenu5.removeAttribute("style", "display:block;");
+      insideMenu6.removeAttribute("style", "display:block;");
+      insideMenu7.removeAttribute("style", "display:block;");
+      insideMenu8.removeAttribute("style", "display:block;");
+      insideMenu9.removeAttribute("style", "display:block;");
+      insideMenu10.removeAttribute("style", "display:block;");
+      insideMenu12.removeAttribute("style", "display:block;");
+      insideMenu13.removeAttribute("style", "display:block;");
+      insideMenu14.removeAttribute("style", "display:block;");
+      insideMenu15.removeAttribute("style", "display:block;");
+    });
+
+    expMenu12.addEventListener('click',function(){
+      console.log('Menu12 open');
+      insideMenu12.setAttribute("style", "display:block;");
+      insideMenu1.removeAttribute("style", "display:block;");
+      insideMenu2.removeAttribute("style", "display:block;");
+      insideMenu3.removeAttribute("style", "display:block;");
+      insideMenu4.removeAttribute("style", "display:block;");
+      insideMenu5.removeAttribute("style", "display:block;");
+      insideMenu6.removeAttribute("style", "display:block;");
+      insideMenu7.removeAttribute("style", "display:block;");
+      insideMenu8.removeAttribute("style", "display:block;");
+      insideMenu9.removeAttribute("style", "display:block;");
+      insideMenu10.removeAttribute("style", "display:block;");
+      insideMenu11.removeAttribute("style", "display:block;");
+      insideMenu13.removeAttribute("style", "display:block;");
+      insideMenu14.removeAttribute("style", "display:block;");
+      insideMenu15.removeAttribute("style", "display:block;");
+    });
+    
+    expMenu13.addEventListener('click',function(){
+      console.log('Menu13 open');
+      insideMenu13.setAttribute("style", "display:block;");
+      insideMenu1.removeAttribute("style", "display:block;");
+      insideMenu2.removeAttribute("style", "display:block;");
+      insideMenu3.removeAttribute("style", "display:block;");
+      insideMenu4.removeAttribute("style", "display:block;");
+      insideMenu5.removeAttribute("style", "display:block;");
+      insideMenu6.removeAttribute("style", "display:block;");
+      insideMenu7.removeAttribute("style", "display:block;");
+      insideMenu8.removeAttribute("style", "display:block;");
+      insideMenu9.removeAttribute("style", "display:block;");
+      insideMenu10.removeAttribute("style", "display:block;");
+      insideMenu11.removeAttribute("style", "display:block;");
+      insideMenu12.removeAttribute("style", "display:block;");
+      insideMenu14.removeAttribute("style", "display:block;");
+      insideMenu15.removeAttribute("style", "display:block;");
+    });
+
+    expMenu14.addEventListener('click',function(){
+      console.log('Menu14 open');
+      insideMenu14.setAttribute("style", "display:block;");
+      insideMenu1.removeAttribute("style", "display:block;");
+      insideMenu2.removeAttribute("style", "display:block;");
+      insideMenu3.removeAttribute("style", "display:block;");
+      insideMenu4.removeAttribute("style", "display:block;");
+      insideMenu5.removeAttribute("style", "display:block;");
+      insideMenu6.removeAttribute("style", "display:block;");
+      insideMenu7.removeAttribute("style", "display:block;");
+      insideMenu8.removeAttribute("style", "display:block;");
+      insideMenu9.removeAttribute("style", "display:block;");
+      insideMenu10.removeAttribute("style", "display:block;");
+      insideMenu11.removeAttribute("style", "display:block;");
+      insideMenu12.removeAttribute("style", "display:block;");
+      insideMenu13.removeAttribute("style", "display:block;");
+      insideMenu15.removeAttribute("style", "display:block;");
+    });
+
+    expMenu15.addEventListener('click',function(){
+      console.log('Menu15 open');
+      insideMenu15.setAttribute("style", "display:block;");
+      insideMenu1.removeAttribute("style", "display:block;");
+      insideMenu2.removeAttribute("style", "display:block;");
+      insideMenu3.removeAttribute("style", "display:block;");
+      insideMenu4.removeAttribute("style", "display:block;");
+      insideMenu5.removeAttribute("style", "display:block;");
+      insideMenu6.removeAttribute("style", "display:block;");
+      insideMenu7.removeAttribute("style", "display:block;");
+      insideMenu8.removeAttribute("style", "display:block;");
+      insideMenu9.removeAttribute("style", "display:block;");
+      insideMenu10.removeAttribute("style", "display:block;");
+      insideMenu11.removeAttribute("style", "display:block;");
+      insideMenu12.removeAttribute("style", "display:block;");
+      insideMenu13.removeAttribute("style", "display:block;");
+      insideMenu14.removeAttribute("style", "display:block;");
+    });
+
+  };
 
 window.addEventListener("turbolinks:load",hide_menu_select);
