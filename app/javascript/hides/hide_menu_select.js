7f22e6b62c3a2cac66284432fce032eaa3aed4ca
kyuu5988 <kyuu5988@yahoo.co.jp>
2020-11-06 17:27:30 +0900

    キャスター

--- app/javascript/hides/hide_menu_select.js
+++ app/javascript/hides/hide_menu_select.js
@@ -7,6 +7,10 @@ function hide_menu_select(){
   const expMenu4 = document.getElementById("exp-menu4")
   const expMenu5 = document.getElementById("exp-menu5")
   const expMenu6 = document.getElementById("exp-menu6")
+  const expMenu7 = document.getElementById("exp-menu7")
+  const expMenu8 = document.getElementById("exp-menu8")
+  const expMenu9 = document.getElementById("exp-menu9")
+  const expMenu10 = document.getElementById("exp-menu10")
 
   const insideMenu1 = document.getElementById("inside-menu1")
   const insideMenu2 = document.getElementById("inside-menu2")
@@ -14,6 +18,10 @@ function hide_menu_select(){
   const insideMenu4 = document.getElementById("inside-menu4")
   const insideMenu5 = document.getElementById("inside-menu5")
   const insideMenu6 = document.getElementById("inside-menu6")
+  const insideMenu7 = document.getElementById("inside-menu7")
+  const insideMenu8 = document.getElementById("inside-menu8")
+  const insideMenu9 = document.getElementById("inside-menu9")
+  const insideMenu10 = document.getElementById("inside-menu10")
 
 
     expMenu1.addEventListener('click',function(){
@@ -51,7 +59,7 @@ function hide_menu_select(){
     });
 
     expMenu4.addEventListener('click',function(){
-      console.log('Menu3 open');
+      console.log('Menu4 open');
       insideMenu4.setAttribute("style", "display:block;");
       insideMenu1.removeAttribute("style", "display:block;");
       insideMenu2.removeAttribute("style", "display:block;");
@@ -62,7 +70,7 @@ function hide_menu_select(){
     });
 
     expMenu5.addEventListener('click',function(){
-      console.log('Menu3 open');
+      console.log('Menu5 open');
       insideMenu5.setAttribute("style", "display:block;");
       insideMenu1.removeAttribute("style", "display:block;");
       insideMenu2.removeAttribute("style", "display:block;");
@@ -71,6 +79,18 @@ function hide_menu_select(){
       insideMenu6.removeAttribute("style", "display:block;");
       insideMenu7.removeAttribute("style", "display:block;");
     });
+
+    expMenu6.addEventListener('click',function(){
+      console.log('Menu6 open');
+      insideMenu6.setAttribute("style", "display:block;");
+      insideMenu1.removeAttribute("style", "display:block;");
+      insideMenu2.removeAttribute("style", "display:block;");
+      insideMenu3.removeAttribute("style", "display:block;");
+      insideMenu4.removeAttribute("style", "display:block;");
+      insideMenu5.removeAttribute("style", "display:block;");
+      insideMenu7.removeAttribute("style", "display:block;");
+    });
+
 
 }
 
